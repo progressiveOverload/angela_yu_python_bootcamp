999c212c89d9c080ce704f1499c30d15ed5de6ad
Ali Cihan Isikal <cihanisikal@outlook.com>
2025-04-30 03:26:15 +0800

    day one finished

---
+++ one.py
@@ -0,0 +1,5 @@
+city =input("Enter the name of the city: ")
+print("The name of the city is: ", city)
+pet =input("Enter the name of the pet: ")
+bandName = city + " " + pet
+print("The band name is: ", bandName)
